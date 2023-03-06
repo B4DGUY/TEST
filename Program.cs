d3de8342f7e64121bb5d8d2e7e13b842ad5f2ed7
B4DGUY <esedov.033@gmail.com>
2023-03-06 21:41:03 +0400

    Test

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string company = "Firma :";
+
+            string adidasCompany = "Adidas";
+            string nikeCompany = "Nike";
+            string gucciCompany = "Gucci";
+            string totalPrice = "Hamisinin birlikde qiymeti ($) :";
+            string numberofshoes = "Ayaqqabi Sayi :";
+            string priceofShoes = "Birinin qiymeti ($) :";
+
+            int adidas = 4;
+            int adidasPrice = 200;
+
+            int gucci = 2;
+            int gucciPrice = 200;
+
+            int nike = 1;
+            int nikePrice = 150;
+
+            float totalAdidas = adidas * adidasPrice;
+            float totalGucci = gucci * gucciPrice;
+            float totalNike = nike * nikePrice;
+
+            Console.WriteLine(company);
+            Console.WriteLine(adidasCompany);
+            Console.WriteLine(numberofshoes);
+            Console.WriteLine(adidas);
+            Console.WriteLine(priceofShoes);
+            Console.WriteLine(adidasPrice);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(totalAdidas);
+
+            Console.WriteLine(company);
+            Console.WriteLine(gucciCompany);
+            Console.WriteLine(numberofshoes);
+            Console.WriteLine(gucci);
+            Console.WriteLine(priceofShoes);
+            Console.WriteLine(adidasPrice);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(totalGucci);
+
+            Console.WriteLine(company);
+            Console.WriteLine(nikeCompany);
+            Console.WriteLine(numberofshoes);
+            Console.WriteLine(nike);
+            Console.WriteLine(priceofShoes);
+            Console.WriteLine(nikePrice);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(totalNike);
+        }
+    }
+}
